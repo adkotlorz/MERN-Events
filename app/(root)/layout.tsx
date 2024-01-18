9a1b066036bf22e2d15bfa18f77901eb33834f48
adkotlorz <ad.kotlorz@gmail.com>
2024-01-18 01:21:33 +0100

    added header and footer. started creatin hero section

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { Footer, Header } from "@/components/shared";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
